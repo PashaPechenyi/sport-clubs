095883e5a1b03399abdffb34b5485610deaf93d9
PashaPechenyi <pashapechenn@gmail.com>
2020-06-22 21:08:14 +0300

    Updates

---
+++ precache-manifest.82acdf32f3b404b4d8bcd0f472609b71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8d8fc1823924e31727b73108896084",
+    "url": "/sport-clubs/index.html"
+  },
+  {
+    "revision": "3e4f45a95e0785693ece",
+    "url": "/sport-clubs/static/css/main.1da30146.chunk.css"
+  },
+  {
+    "revision": "719b83e4379891c28f1e",
+    "url": "/sport-clubs/static/js/2.176f2f5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sport-clubs/static/js/2.176f2f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4f45a95e0785693ece",
+    "url": "/sport-clubs/static/js/main.b1c8b105.chunk.js"
+  },
+  {
+    "revision": "7f76d6ad27641596f0de",
+    "url": "/sport-clubs/static/js/runtime-main.460475b6.js"
+  },
+  {
+    "revision": "f98b32ceb4e65ec11ae685fc4b6d5a15",
+    "url": "/sport-clubs/static/media/default-image.f98b32ce.png"
+  },
+  {
+    "revision": "66e9972c99052326436511e3f5841c47",
+    "url": "/sport-clubs/static/media/logo.66e9972c.svg"
+  }
+]);
